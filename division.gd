dffe8f195f37145d42f0d755acc509b3bcb3cd4a
carbro360 <carbro360@gmail.com>
2023-06-08 01:35:18 -0400

    cursor

---
+++ division.gd
@@ -0,0 +1,24 @@
+#class_name division
+
+var attack : float #decreases as strength decreases
+var defense : float #decreases as strength decreases
+var speed : float
+var max_strength : float #based on manpower and supply
+var actual_strength : float
+var morale : float #based on unit supply, damage being taken, and terrain modifiers
+var supply : int
+var manpower : int
+var defense_mod: float #based on terrain
+
+var unit : Array = [attack, defense, speed, max_strength, actual_strength, morale, supply, manpower, defense_mod]
+
+
+
+#larger units move slower through difficult terrain (at least until modern era)
+#
+static func combat(unitA: Array, unitB : Array):
+	while (unitA[5] > .85 && unitB[5] > .85):
+		print("balls")
+
+func move(unit: Array):
+	pass
